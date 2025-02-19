12fdc4a2d798840b496c0f38f5908293fbfce8f5
Zujuy <itza_zujuy@hotmail.co.uk>
2025-02-18 23:19:09 -0600

    fixing app.js

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -2,22 +2,22 @@
 import React from 'react';
 import GlobalStyles from './styles/GlobalStyles';
 import Description from './components/Description/Description';
-// import Experience from './components/Experience/Experience';
-// import Conferences from './components/Conferences/Conferences';
-// import Communities from './components/Communities/Communities';
-// import Favorites from './components/Favorites/Favorites';
-// import ContactForm from './components/ContactForm/ContactForm';
+import Experience from './components/Experience/Experience';
+import Conferences from './components/Conferences/Conferences';
+import Communities from './components/Communities/Communities';
+import Favorites from './components/Favorites/Favorites';
+import ContactForm from './components/ContactForm/ContactForm';
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
       <Description />
-      {/* <Experience />
+      <Experience />
       <Conferences />
       <Communities />
       <Favorites />
-      <ContactForm /> */}
+      <ContactForm />
     </>
   );
 };
